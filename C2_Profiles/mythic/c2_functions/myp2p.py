676856b4092520ddad8ab325160db1006f3e3b8b
simonedaini <simonedaini1993@gmail.com>
2021-09-01 11:30:26 +0200

    myp2p init

---
+++ C2_Profiles/mythic/c2_functions/myp2p.py
@@ -0,0 +1,10 @@
+from mythic_c2_container.C2ProfileBase import *
+
+class Myp2pProfile(C2Profile):
+    name = "myp2p"
+    description = "p2p profile between agents"
+    author = "@Kayn93"
+    is_p2p = True
+    is_server_routed = True
+    notes = ""
+    parameters = []
